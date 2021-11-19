def9e63b795ef1e01732fbddc122c05df0dccdb2
TrieuVD <trieuvd@rikkeisoft.com>
2021-11-19 18:05:53 +0700

    add login script

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:html/parser.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,6 +33,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  InAppWebViewController? iawControlller;
+  final TextEditingController userController = TextEditingController();
+  final TextEditingController passController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,21 +48,57 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             Expanded(
               child: InAppWebView(
-                initialUrlRequest: URLRequest(url: Uri.parse("https://ib.techcombank.com.vn/servlet/BrowserServlet"))
+                initialUrlRequest: URLRequest(
+                    url: Uri.parse(
+                        "https://ib.techcombank.com.vn/servlet/BrowserServlet")),
+                onWebViewCreated: (InAppWebViewController controller) {
+                  iawControlller = controller;
+                },
               ),
             ),
             const SizedBox(
               height: 200,
             ),
-            const TextField(),
+            TextField(controller: userController),
             const SizedBox(
               height: 20,
             ),
-            const TextField(),
+            TextField(controller: passController,),
             const SizedBox(
               height: 50,
             ),
-            ElevatedButton(onPressed: () {}, child: const Text("Login")),
+            Row(
+              children: [
+                ElevatedButton(
+                    onPressed: () async {
+                      iawControlller?.evaluateJavascript(source: "javascript:\$('#signOnName').val('01627093838');");
+                      iawControlller?.evaluateJavascript(source: "javascript:\$('#password').val('Hoilg12!');");
+                      iawControlller?.evaluateJavascript(source: "javascript:\$(\"form[name='login']\").submit();");
+                    },
+                    child: const Text("Login")),
+
+                ElevatedButton(
+                    onPressed: () async {
+                      iawControlller?.evaluateJavascript(source: "javascript:document.getElementsByName('AI.QCK.ACCOUNT')[0].click()");
+                    },
+                    child: const Text("chuyển tab tk")),
+                ElevatedButton(
+                    onPressed: () async {
+                      iawControlller?.evaluateJavascript(source: "javascript:\$(\"a[title='Xem giao dịch']\")[0].click();");
+                    },
+                    child: const Text("lấy gd")),
+
+                ElevatedButton(
+                    onPressed: () async {
+                      var htmlStr = await iawControlller?.evaluateJavascript(source: "window.document.getElementsByTagName('html')[0].outerHTML;");
+                      print("trieuvd content html: $htmlStr");
+                      var document = parse(htmlStr);
+                      print("content html: ${document.getElementById("cv_main")?.innerHtml}");
+
+                    },
+                    child: const Text("show gd")),
+              ],
+            )
           ],
         ),
       ),
