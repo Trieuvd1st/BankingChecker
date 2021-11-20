05ab36e17fd6f36e5e8f570b34e15b15dfdd901f
TrieuVD <trieuvd@rikkeisoft.com>
2021-11-20 18:05:47 +0700

    fhsdklfjdlfja

--- lib/main.dart
+++ lib/main.dart
@@ -114,9 +114,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: const Text("chuyển tab tk")),
                 ElevatedButton(
                     onPressed: () async {
-                      iawControlller?.evaluateJavascript(
-                          source:
-                              "javascript:\$(\"#appreq_STMTSTEPTWO905900202101 a[title='Xem giao dịch']\")[0].click();");
+                      //await iawControlller?.evaluateJavascript(source: "javascript:\$(\"table[id='goButton']:eq(1) a[title='Xem giao dịch']\").click();");
+                      iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:START.DATE']\").val('01/09/2021');");
+                      iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:END.DATE']\").val('01/11/2021');");
+                      iawControlller?.evaluateJavascript(source: "javascript:\$(\"a[title='Xem giao dịch']\")[0].click();");
                     },
                     child: const Text("lấy gd")),
                 ElevatedButton(
