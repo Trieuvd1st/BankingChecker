56dcc1dfaeb9ad931579bb35002d1d7468ca518b
TrieuvdInHome <trieuvu.ptit@gmail.com>
2021-11-23 00:29:05 +0700

    add logic auto click webview

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,6 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController passController = TextEditingController();
   String textTransaction = "";
   int getTransactionStep = 0;
-  bool hasFocusWv = false;
-  FocusNode? focusNoteWv;
 
   void _setTextTransaction(String text) {
     setState(() {
@@ -46,11 +44,105 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _setFocus(bool hasFocus) {
+  void onTapDown(BuildContext context, TapDownDetails details) {
+    print('${details.globalPosition}');
+    final RenderBox box = context.findRenderObject();
+    final Offset localOffset = box.globalToLocal(details.globalPosition);
     setState(() {
-      hasFocusWv = hasFocus;
+      posx = localOffset.dx;
+      posy = localOffset.dy;
     });
   }
+
+  Widget _dalkjfsdf(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: InAppWebView(
+            initialUrlRequest: URLRequest(
+                url: Uri.parse(
+                    "https://ib.techcombank.com.vn/servlet/BrowserServlet")),
+            onWebViewCreated: (InAppWebViewController controller) {
+              iawControlller = controller;
+            },
+            onLoadStop: (InAppWebViewController controller, Uri? url) async {
+              print("get step $getTransactionStep");
+              if (getTransactionStep == 0) {
+                await iawControlller?.evaluateJavascript(
+                    source:
+                    "javascript:document.getElementsByName('AI.QCK.ACCOUNT')[0].click()");
+                getTransactionStep++;
+              }
+            },
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(textTransaction),
+        const SizedBox(
+          height: 100,
+        ),
+        TextField(controller: userController),
+        const SizedBox(
+          height: 50,
+        ),
+        TextField(
+          controller: passController,
+          autofocus: false,
+        ),
+        const SizedBox(
+          height: 40,
+        ),
+        Row(
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  iawControlller?.evaluateJavascript(
+                      source:
+                      "javascript:\$('#signOnName').val('01627093838');");
+                  iawControlller?.evaluateJavascript(
+                      source: "javascript:\$('#password').val('Hoilg12!');");
+                  iawControlller?.evaluateJavascript(
+                      source:
+                      "javascript:\$(\"form[name='login']\").submit();");
+                },
+                child: const Text("Login")),
+            ElevatedButton(
+                onPressed: () async {
+                  iawControlller?.evaluateJavascript(
+                      source:
+                      "javascript:document.getElementsByName('AI.QCK.ACCOUNT')[0].click()");
+                },
+                child: const Text("chuyển tab tk")),
+            ElevatedButton(
+                onPressed: () async {
+                  iawControlller?.evaluateJavascript(
+                      source:
+                      "javascript:\$(\"a[title='Xem giao dịch']\")[0].click();");
+                  //iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:START.DATE']\").focus();");
+                  // iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:START.DATE']\").val('01/09/2021');");
+                  // iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:END.DATE']\").val('01/11/2021');");
+                },
+                child: const Text("lấy gd")),
+            ElevatedButton(
+                onPressed: () async {
+                  var htmlStr = await iawControlller?.evaluateJavascript(
+                      source:
+                      "window.document.getElementsByTagName('html')[0].outerHTML;");
+                  //print("trieuvd content html: $htmlStr");
+                  var document = parse(htmlStr);
+                  //print("content html: ${document.getElementsByClassName('colour1')[0].getElementsByTagName('td')[1].innerHtml}");
+                  _setTextTransaction(document
+                      .getElementsByClassName('colour1')[0]
+                      .getElementsByTagName('td')[1]
+                      .innerHtml);
+                },
+                child: const Text("show gd")),
+          ],
+        ),
+      ],
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -59,96 +151,8 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          children: [
-            Expanded(
-              child: Focus(
-                focusNode: focusNoteWv,
-                child: InAppWebView(
-                  initialUrlRequest: URLRequest(
-                      url: Uri.parse(
-                          "https://ib.techcombank.com.vn/servlet/BrowserServlet")),
-                  onWebViewCreated: (InAppWebViewController controller) {
-                    iawControlller = controller;
-                  },
-
-                  onLoadStop: (InAppWebViewController controller, Uri? url) async {
-                    print("get step $getTransactionStep");
-                    if(getTransactionStep == 0) {
-                      await iawControlller?.evaluateJavascript(
-                          source:
-                          "javascript:document.getElementsByName('AI.QCK.ACCOUNT')[0].click()");
-                      getTransactionStep++;
-                    }
-                  },
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Text(textTransaction),
-            const SizedBox(
-              height: 100,
-            ),
-            TextField(controller: userController),
-            const SizedBox(
-              height: 50,
-            ),
-            TextField(
-              controller: passController,
-              autofocus: false,
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            Row(
-              children: [
-                ElevatedButton(
-                    onPressed: () async {
-                      iawControlller?.evaluateJavascript(
-                          source:
-                              "javascript:\$('#signOnName').val('01627093838');");
-                      iawControlller?.evaluateJavascript(
-                          source:
-                              "javascript:\$('#password').val('Hoilg12!');");
-                      iawControlller?.evaluateJavascript(
-                          source:
-                              "javascript:\$(\"form[name='login']\").submit();");
-                    },
-                    child: const Text("Login")),
-                ElevatedButton(
-                    onPressed: () async {
-                      iawControlller?.evaluateJavascript(
-                          source:
-                              "javascript:document.getElementsByName('AI.QCK.ACCOUNT')[0].click()");
-                      _setFocus(true);
-                    },
-                    child: const Text("chuyển tab tk")),
-                ElevatedButton(
-                    onPressed: () async {
-                      Focus.of(context).requestFocus(focusNoteWv);
-                      iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:START.DATE']\").focus();");
-                      // iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:START.DATE']\").val('01/09/2021');");
-                      // iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:END.DATE']\").val('01/11/2021');");
-                    },
-                    child: const Text("lấy gd")),
-                ElevatedButton(
-                    onPressed: () async {
-                      iawControlller?.evaluateJavascript(source: "javascript:\$(\"a[title='Xem giao dịch']\")[0].click();");
-                      // var htmlStr = await iawControlller?.evaluateJavascript(
-                      //     source:
-                      //         "window.document.getElementsByTagName('html')[0].outerHTML;");
-                      // //print("trieuvd content html: $htmlStr");
-                      // var document = parse(htmlStr);
-                      // //print("content html: ${document.getElementsByClassName('colour1')[0].getElementsByTagName('td')[1].innerHtml}");
-                      // _setTextTransaction(document
-                      //     .getElementsByClassName('colour1')[0]
-                      //     .getElementsByTagName('td')[1]
-                      //     .innerHtml);
-                    },
-                    child: const Text("show gd")),
-              ],
-            ),
-          ],
+        child: GestureDetector(
+          child: _dalkjfsdf(context),
         ),
       ),
     );
