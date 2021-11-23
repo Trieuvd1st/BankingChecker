02cdea56f7c6e1a19130e672e3562f6888e341bc
TrieuvdInHome <trieuvu.ptit@gmail.com>
2021-11-24 00:55:24 +0700

    jgkhkkljl

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController passController = TextEditingController();
   String textTransaction = "";
   int getTransactionStep = 0;
+  GestureDetector? gestureDetector;
 
   void _setTextTransaction(String text) {
     setState(() {
@@ -44,37 +45,52 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void onTapDown(BuildContext context, TapDownDetails details) {
-    print('${details.globalPosition}');
-    final RenderBox box = context.findRenderObject();
-    final Offset localOffset = box.globalToLocal(details.globalPosition);
-    setState(() {
-      posx = localOffset.dx;
-      posy = localOffset.dy;
-    });
+  @override
+  void initState() {
+    super.initState();
+
+    gestureDetector = GestureDetector(
+      onTap: () {
+        print("TRIEUVD: onTap webview");
+      },
+      child: Container(
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+              url: Uri.parse(
+                  "https://ib.techcombank.com.vn/servlet/BrowserServlet")),
+          onWebViewCreated: (InAppWebViewController controller) {
+            iawControlller = controller;
+          },
+          onLoadStop: (InAppWebViewController controller, Uri? url) async {
+            print("get step $getTransactionStep");
+            if (getTransactionStep == 0) {
+              await iawControlller?.evaluateJavascript(
+                  source:
+                      "javascript:document.getElementsByName('AI.QCK.ACCOUNT')[0].click()");
+              getTransactionStep++;
+            }
+          },
+        ),
+      ),
+    );
   }
+
+  // void onTapDown(BuildContext context, TapDownDetails details) {
+  //   print('${details.globalPosition}');
+  //   final RenderBox box = context.findRenderObject();
+  //   final Offset localOffset = box.globalToLocal(details.globalPosition);
+  //   setState(() {
+  //     posx = localOffset.dx;
+  //     posy = localOffset.dy;
+  //   });
+  // }
 
   Widget _dalkjfsdf(BuildContext context) {
     return Column(
       children: [
         Expanded(
-          child: InAppWebView(
-            initialUrlRequest: URLRequest(
-                url: Uri.parse(
-                    "https://ib.techcombank.com.vn/servlet/BrowserServlet")),
-            onWebViewCreated: (InAppWebViewController controller) {
-              iawControlller = controller;
-            },
-            onLoadStop: (InAppWebViewController controller, Uri? url) async {
-              print("get step $getTransactionStep");
-              if (getTransactionStep == 0) {
-                await iawControlller?.evaluateJavascript(
-                    source:
-                    "javascript:document.getElementsByName('AI.QCK.ACCOUNT')[0].click()");
-                getTransactionStep++;
-              }
-            },
-          ),
+          child:
+              Opacity(opacity: 0, child: gestureDetector ?? const SizedBox()),
         ),
         const SizedBox(height: 20),
         Text(textTransaction),
@@ -85,40 +101,38 @@ class _MyHomePageState extends State<MyHomePage> {
         const SizedBox(
           height: 50,
         ),
-        TextField(
-          controller: passController,
-          autofocus: false,
-        ),
-        const SizedBox(
-          height: 40,
-        ),
+        TextField(controller: passController),
+        const SizedBox(height: 40),
         Row(
           children: [
             ElevatedButton(
                 onPressed: () async {
                   iawControlller?.evaluateJavascript(
                       source:
-                      "javascript:\$('#signOnName').val('01627093838');");
+                          "javascript:\$('#signOnName').val('01627093838');");
                   iawControlller?.evaluateJavascript(
                       source: "javascript:\$('#password').val('Hoilg12!');");
                   iawControlller?.evaluateJavascript(
                       source:
-                      "javascript:\$(\"form[name='login']\").submit();");
+                          "javascript:\$(\"form[name='login']\").submit();");
                 },
                 child: const Text("Login")),
             ElevatedButton(
                 onPressed: () async {
                   iawControlller?.evaluateJavascript(
                       source:
-                      "javascript:document.getElementsByName('AI.QCK.ACCOUNT')[0].click()");
+                          "javascript:document.getElementsByName('AI.QCK.ACCOUNT')[0].click()");
+                  gestureDetector?.onTap;
                 },
                 child: const Text("chuyển tab tk")),
             ElevatedButton(
                 onPressed: () async {
+                  // iawControlller?.evaluateJavascript(
+                  //     source:
+                  //         "javascript:\$(\"a[title='Xem giao dịch']\")[0].click();");
                   iawControlller?.evaluateJavascript(
                       source:
-                      "javascript:\$(\"a[title='Xem giao dịch']\")[0].click();");
-                  //iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:START.DATE']\").focus();");
+                          "javascript:\$(\"input[name='fieldName:START.DATE']\").focus();");
                   // iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:START.DATE']\").val('01/09/2021');");
                   // iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:END.DATE']\").val('01/11/2021');");
                 },
@@ -127,7 +141,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 onPressed: () async {
                   var htmlStr = await iawControlller?.evaluateJavascript(
                       source:
-                      "window.document.getElementsByTagName('html')[0].outerHTML;");
+                          "window.document.getElementsByTagName('html')[0].outerHTML;");
                   //print("trieuvd content html: $htmlStr");
                   var document = parse(htmlStr);
                   //print("content html: ${document.getElementsByClassName('colour1')[0].getElementsByTagName('td')[1].innerHtml}");
@@ -142,7 +156,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
