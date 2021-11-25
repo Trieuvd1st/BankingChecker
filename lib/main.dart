c3a89575b0d8eed40ee2c26883548fab6b4420ba
TrieuVD <trieuvd@rikkeisoft.com>
2021-11-25 18:13:00 +0700

    add download csv

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,10 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController userController = TextEditingController();
   final TextEditingController passController = TextEditingController();
   String textTransaction = "";
-  GestureDetector? gestureDetector;
+  bool loginIsVisible = true;
+  double wvOpacity = 0;
+  int stepLoading = 0;
+  bool isEnableLogin = false;
 
   void _setTextTransaction(String text) {
     setState(() {
@@ -46,108 +49,113 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _setLoginVisible(bool state) {
+    setState(() {
+      loginIsVisible = state;
+    });
+  }
+
+  void _setWvVisible(double value) {
+    setState(() {
+      wvOpacity = value;
+    });
+  }
+
+  void _setEnableLogin(bool state) {
+    setState(() {
+      isEnableLogin = state;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    FlutterDownloader.initialize();
   }
 
   Widget _dalkjfsdf(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        Expanded(
-          child:
-              Opacity(opacity: 1, child: InAppWebView(
-                initialUrlRequest: URLRequest(
-                    url: Uri.parse(
-                        "https://ib.techcombank.com.vn/servlet/BrowserServlet")),
-                onWebViewCreated: (InAppWebViewController controller) {
-                  iawControlller = controller;
-                },
-                initialOptions: InAppWebViewGroupOptions(
-                  crossPlatform: InAppWebViewOptions(
-                      useOnDownloadStart: true
-                  ),
+        Opacity(
+          opacity: wvOpacity,
+          child: InAppWebView(
+            initialUrlRequest: URLRequest(
+                url: Uri.parse(
+                    "https://ib.techcombank.com.vn/servlet/BrowserServlet")),
+            onWebViewCreated: (InAppWebViewController controller) {
+              iawControlller = controller;
+            },
+            initialOptions: InAppWebViewGroupOptions(
+              crossPlatform: InAppWebViewOptions(
+                javaScriptEnabled: true,
+                  useOnDownloadStart: true
+              ),
+            ),
+            onDownloadStart: (controller, uri) async {
+              print("onDownloadStart $uri");
+              final taskId = await FlutterDownloader.enqueue(
+                url: "",
+                savedDir: (await getExternalStorageDirectory())!.path,
+                showNotification: true,
+                // show download progress in status bar (for Android)
+                openFileFromNotification:
+                    true, // click on notification to open downloaded file (for Android)
+              );
+            },
+            onLoadStop: (InAppWebViewController controller, Uri? url) async {
+              stepLoading++;
+              print("loading onstop $stepLoading");
+              if (stepLoading == 1) {
+                _setEnableLogin(true);
+              }
+              if (stepLoading == 2) {
+                _setLoginVisible(false);
+                _setWvVisible(1);
+              }
+              if (stepLoading == 3) {
+                await iawControlller?.evaluateJavascript(
+                    source:
+                        "javascript:document.getElementsByName('AI.QCK.ACCOUNT')[0].click()");
+              }
+              if (stepLoading == 4) {
+                print("TRIEUVD: da vao man giao dich");
+                await iawControlller?.evaluateJavascript(
+                    source:
+                    "javascript:\$(\"input[name='fieldName:START.DATE']\").val('01/09/2021');");
+                await iawControlller?.evaluateJavascript(
+                    source:
+                    "javascript:\$(\"input[name='fieldName:END.DATE']\").val('01/11/2021');");
+              }
+            },
+          ),
+        ),
+        Visibility(
+            visible: loginIsVisible,
+            child: Column(
+              children: [
+              const SizedBox(height: 50,),
+                TextField(controller: userController),
+                const SizedBox(
+                  height: 50,
                 ),
-                onDownloadStart: (controller, uri) async {
-                  print("onDownloadStart $uri");
-                  final taskId = await FlutterDownloader.enqueue(
-                      url: uri.path,
-                      savedDir: (await getExternalStorageDirectory())!.path,
-                  showNotification: true, // show download progress in status bar (for Android)
-                  openFileFromNotification: true, // click on notification to open downloaded file (for Android)
-                  );
-                },
-                onLoadStop: (InAppWebViewController controller, Uri? url) async {
-                  // if (getTransactionStep == 0) {
-                  //   await iawControlller?.evaluateJavascript(
-                  //       source:
-                  //       "javascript:document.getElementsByName('AI.QCK.ACCOUNT')[0].click()");
-                  //   getTransactionStep++;
-                  //}
-                },
-              ),),
-        ),
-        const SizedBox(height: 20),
-        Text(textTransaction),
-        const SizedBox(
-          height: 100,
-        ),
-        TextField(controller: userController),
-        const SizedBox(
-          height: 50,
-        ),
-        TextField(controller: passController),
-        const SizedBox(height: 40),
-        Row(
-          children: [
-            ElevatedButton(
-                onPressed: () async {
-                  iawControlller?.evaluateJavascript(
-                      source:
-                          "javascript:\$('#signOnName').val('01627093838');");
-                  iawControlller?.evaluateJavascript(
-                      source: "javascript:\$('#password').val('Hoilg12!');");
-                  iawControlller?.evaluateJavascript(
-                      source:
-                          "javascript:\$(\"form[name='login']\").submit();");
-                },
-                child: const Text("Login")),
-            ElevatedButton(
-                onPressed: () async {
-                  iawControlller?.evaluateJavascript(
-                      source:
-                          "javascript:document.getElementsByName('AI.QCK.ACCOUNT')[0].click()");
-                  gestureDetector?.onTap;
-                },
-                child: const Text("chuyển tab tk")),
-            ElevatedButton(
-                onPressed: () async {
-                  iawControlller?.evaluateJavascript(
-                      source:
-                          "javascript:\$(\"a[title='Xem giao dịch']\")[0].click();");
-                  // iawControlller?.evaluateJavascript(
-                  //     source:
-                  //         "javascript:\$(\"input[name='fieldName:START.DATE']\").focus();");
-                  iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:START.DATE']\").val('01/09/2021');");
-                  iawControlller?.evaluateJavascript(source: "javascript:\$(\"input[name='fieldName:END.DATE']\").val('01/11/2021');");
-                },
-                child: const Text("lấy gd")),
-            ElevatedButton(
-                onPressed: () async {
-                  var htmlStr = await iawControlller?.evaluateJavascript(
-                      source:
-                          "window.document.getElementsByTagName('html')[0].outerHTML;");
-                  //print("trieuvd content html: $htmlStr");
-                  var document = parse(htmlStr);
-                  //print("content html: ${document.getElementsByClassName('colour1')[0].getElementsByTagName('td')[1].innerHtml}");
-                  _setTextTransaction(document
-                      .getElementsByClassName('colour1')[0]
-                      .getElementsByTagName('td')[1]
-                      .innerHtml);
-                },
-                child: const Text("show gd")),
-          ],
-        ),
+                TextField(controller: userController),
+                const SizedBox(height: 40),
+                ElevatedButton(
+                    onPressed: !isEnableLogin? null : () async {
+                      iawControlller?.evaluateJavascript(
+                          source:
+                              "javascript:\$('#signOnName').val('01627093838');");
+                      iawControlller?.evaluateJavascript(
+                          source:
+                              "javascript:\$('#password').val('Hoilg12!');");
+                      iawControlller?.evaluateJavascript(
+                          source:
+                              "javascript:\$(\"form[name='login']\").submit();");
+                    },
+                    child: const Text("Login")),
+              ],
+            ))
+
       ],
     );
   }
